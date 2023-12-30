a25baacae6ced4a01f8ea2ebfe49650df6b489d8
RakerZh <gitrustz10@gmail.com>
2023-12-29 23:28:54 -0500

    update bin

--- emfd/main.py
+++ emfd/main.py
@@ -19,8 +19,7 @@ parser.add_argument('score_method', metavar='scoring_method', nargs='+', type=st
                     help='Text scoring method. Possible values are: bow, wordlist, gdelt.ngrams, pat')
                     
 parser.add_argument('output_metrics', metavar='output_metrics', nargs='+', type=str, default='sentiment',
-                    help='eMFD only. Either returns an average sentiment score for each foundation (sentiment) or splits each foundation into a vice/virtue category (vice-virtue).
-                    Possible values are: sentiment, vice-virtue')
+                    help='eMFD only. Either returns an average sentiment score for each foundation (sentiment) or splits each foundation into a vice/virtue category (vice-virtue). Possible values are: sentiment, vice-virtue')
 
 parser.add_argument('output_csv', metavar='outfile', nargs='+', type=str,
                     help='The path/name for the scored output CSV.')
@@ -35,10 +34,17 @@ OUT_CSV_PATH = args['output_csv'][0]
 
 infile_type = IN_CSV_PATH.split('.')[-1]
 
+num_docs = 0
+d = {'empty': [0]}
+csv = pd.DataFrame(data=d)
+df = pd.DataFrame(data=d)
+
 if infile_type == 'csv':
     csv = pd.read_csv(IN_CSV_PATH, header=None)
     num_docs = len(csv)
 
+    print("Running emfd")
+    print("Total number of input texts to be scored:", num_docs)
 elif infile_type == 'txt':
     ngrams =  open(IN_CSV_PATH).readlines()
     df = pd.DataFrame()
@@ -48,23 +54,23 @@ elif infile_type == 'txt':
 else:
     print('Input file type not recognized! Must either be CSV for scoring method bow, wordlist, and pat, or TXT for gdelt.ngram')
 
-print("Running emfd")
-print("Total number of input texts to be scored:", num_docs)
+if num_docs == 0:
+    print("There are no data in your file. Check if it is loaded")
+else:
+    if SCORE_METHOD == 'bow':
+        df = score_docs(csv,DICT_TYPE,PROB_MAP,SCORE_METHOD,OUT_METRICS,num_docs)
+        df.to_csv(OUT_CSV_PATH, index=False)
 
-if SCORE_METHOD == 'bow':
-    df = score_docs(csv,DICT_TYPE,PROB_MAP,SCORE_METHOD,OUT_METRICS,num_docs)
-    df.to_csv(OUT_CSV_PATH, index=False)
+    if SCORE_METHOD == 'wordlist':
+        df = score_docs(csv,DICT_TYPE,PROB_MAP,SCORE_METHOD,OUT_METRICS,num_docs)
+        df.to_csv(OUT_CSV_PATH, index=False)
 
-if SCORE_METHOD == 'wordlist':
-    df = score_docs(csv,DICT_TYPE,SCORE_METHOD,num_docs)
-    df.to_csv(OUT_CSV_PATH, index=False)
+    if SCORE_METHOD == 'gdelt.ngrams':
+        df = score_docs(csv,DICT_TYPE,PROB_MAP,SCORE_METHOD,OUT_METRICS,num_docs)
+        df.to_csv(OUT_CSV_PATH, index=False)
 
-if SCORE_METHOD == 'gdelt.ngrams':
-    df = score_docs(df,DICT_TYPE,SCORE_METHOD,num_docs)
-    df.to_csv(OUT_CSV_PATH, index=False)
-
-if SCORE_METHOD == 'pat':
-    df = pat_docs(csv,num_docs)
-    df.to_csv(OUT_CSV_PATH, index=False)
+    if SCORE_METHOD == 'pat':
+        df = pat_docs(csv,num_docs)
+        df.to_csv(OUT_CSV_PATH, index=False)
 
 print('Scoring completed.')
